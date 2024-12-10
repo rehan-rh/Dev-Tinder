e875a5846f2de644e34df5b9c8d0f5e66cb72c07
rehan-rh <rehanhussain0842@gmail.com>
2024-12-10 23:18:34 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -6,24 +6,26 @@ const app = express();
 
 const { adminAuth, userAuth } = require("./middlewares/auth.js")
 
-app.use("/admin", adminAuth);
 
-app.get("/user", userAuth,  (req, res)=>{
+app.get("/getUserData",  (req, res)=>{
+    try{
     res.send("All user data sent");
+    }
+    catch(err){
+        res.status(401).send("something went wrong....");
+    }
+    
 });
 
-app.get("/admin/getAllData", (req, res)=>{
-    res.send("All data sent");
-});
+//error handling - proper way is try and catch but keep this code at the end
+// app.use("/", (err, req, res, next)=>{
+//     if(err)
+//     {
+//         res.status(500).send("something went wrong....");
+//     }
 
-app.get("/admin/deleteUser", (req, res)=>{
-    res.send("Deleted a user");
-});
+// });
 
-
-app.use("/", (req, res)=>{//this function is route handler
-   // res.send("test from the server....")
-})
 
 //server is listenig the requests
 app.listen(3000, ()=>{
