4014a3a6f0de9688ab7eb98e67c46ac171fbe4c6
rehan-rh <rehanhussain0842@gmail.com>
2024-12-11 17:27:42 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -1,42 +1,28 @@
 const express = require("express");
 const connectDB = require("./cosnfig/database");
 const app = express();
-const User = require("./models/user")
+const User = require("./models/user");
+app.use(express.json()); //this is middleware converts the json format to js object
 
-app.post("/signup", async(req, res)=>{
-    const userObj = {
-        firstName: "saniya",
-        lastName: "begum",
-        emailId: "rehan@gmail.com",
-        password: "rehan123"
-
-    }
-    //creating new insance of the user model
-    const user = new User(userObj);
-    try{
-        await user.save();
+app.post("/signup", async (req, res) => {
+  //creating new insance of the user model
+  const user = new User(req.body);
+  try {
+    await user.save();
     res.send("User added successfully...");
-    }
-    catch(err){
-        res.status(400).send("Something went wrong...", arr.message);
-    }
-    
-
-    
-})
-
-
+  } catch (err) {
+    res.status(400).send("Something went wrong...", arr.message);
+  }
+});
 
 // first connect the database then start listening the server
 connectDB()
   .then(() => {
     console.log("Database connected successfully...");
     app.listen(3000, () => {
-        console.log("server is successfully listening....");
-      });
+      console.log("server is successfully listening....");
+    });
   })
   .catch((err) => {
     console.error("Database cannot be connected...!!");
   });
-
-
