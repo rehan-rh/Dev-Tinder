2bbaca5cca0160e0ef32becebe24c405bc45fafa
Rehan Hussain <rehanhussain0842@email.com>
2025-07-14 14:45:14 +0530

    cookie probem fixed

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,7 +5,6 @@ const { validateSignUpData } = require("../utils/validation");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
-
 authRouter.post("/signup", async (req, res) => {
   try {
     //validation of data
@@ -32,43 +31,45 @@ authRouter.post("/signup", async (req, res) => {
 });
 
 authRouter.post("/login", async (req, res) => {
-    try {
-      const { emailId, password } = req.body;
-  
-      const user = await User.findOne({ emailId: emailId });
-      if (!user) {
-        throw new Error("Invalid credentials...!");
-      }
-  
-      const isPasswordValid = await user.validatePassword(password);
-  
-      if (isPasswordValid) {
-        //create JWT token....
-        //npm documentations
-        const token = await user.getJWT();
-        //console.log(token);
-  
-        //add the token to cookies and send the response back to the user....
-        // from expressjs.com documentation
-        res.cookie("token", token, {
-          expires: new Date(Date.now() + 8 * 3600000),
-        });
-  
-        res.send(user);
-      } else {
-        throw new Error("Invalid credentials...!");
-      }
-    } catch (err) {
-      res.status(400).send("Error : " + err.message);
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials...!");
     }
+
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+      //create JWT token....
+      //npm documentations
+      const token = await user.getJWT();
+      //console.log(token);
+
+      //add the token to cookies and send the response back to the user....
+      // from expressjs.com documentation
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false, // ✅ false in local, true in production (HTTPS)
+        sameSite: "Lax", // ✅ 'Lax' for localhost, 'None' for different domains
+        expires: new Date(Date.now() + 8 * 3600000), // 8 hours
+      });
+
+      res.send(user);
+    } else {
+      throw new Error("Invalid credentials...!");
+    }
+  } catch (err) {
+    res.status(400).send("Error : " + err.message);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
   });
 
-authRouter.post("/logout", async (req, res)=>{
-    res.cookie("token", null, {
-        expires: new Date(Date.now()),
-    });
-
-    res.send("Logged out successfull...!");
-    
-})
+  res.send("Logged out successfull...!");
+});
 module.exports = authRouter;
