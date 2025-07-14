232e495e2de5935dd694ba217afa6934868774cf
Rehan Hussain <rehanhussain0842@email.com>
2025-07-14 16:07:59 +0530

    ..

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -50,9 +50,9 @@ authRouter.post("/login", async (req, res) => {
       //add the token to cookies and send the response back to the user....
       // from expressjs.com documentation
       res.cookie("token", token, {
-        httpOnly: false,
-        secure: true, // ✅ Required for HTTPS (production)
-        sameSite: "None", // ✅ Required for cross-site
+        httpOnly: true,
+        secure: true, // 🔥 Required for HTTPS
+        sameSite: "None", // 🔥 Required for cross-site
         expires: new Date(Date.now() + 8 * 3600000),
       });
 
